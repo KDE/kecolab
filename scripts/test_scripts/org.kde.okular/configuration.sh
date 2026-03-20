d6aac3674552affe99c55bca09b3080545848e14
Hrishikesh Gohain <hrishikeshgohain123@gmail.com>
2026-03-21 03:18:31 +0530

    fix: fix shortcuts file path for flatpaks
    the old path for importing new shortcuts was for system package manager installed apps
    update it with flatpak app data paths
    
    Signed-off-by: Hrishikesh Gohain <hrishikeshgohain123@gmail.com>

--- scripts/test_scripts/org.kde.okular/configuration.sh
+++ scripts/test_scripts/org.kde.okular/configuration.sh
@@ -3,8 +3,9 @@
 ## Setting custom shortcuts
 echo "Importing shortcuts"
 sleep 1
-rm ~/.local/share/kxmlgui5/okular/part.rc
+rm ~/.var/app/org.kde.okular/data/kxmlgui5/okular/part.rc
 sleep 1
-cp part.rc ~/.local/share/kxmlgui5/okular/part.rc
+mkdir -p ~/.var/app/org.kde.okular/data/kxmlgui5/okular/
+cp part.rc ~/.var/app/org.kde.okular/data/kxmlgui5/okular/part.rc
 sleep 1
 echo "Finished importing shortcuts"

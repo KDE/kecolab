524b983998b20f5b5f28838061d4ff545a4a1229
Joseph P. De Veaugh-Geiss <joseph@kde.org>
2026-06-11 10:25:25 +0200

    reduced iterations to 2; burn in and cool down times to 10 seconds

--- scripts/test_scripts/org.kde.okular/log_sus.sh
+++ scripts/test_scripts/org.kde.okular/log_sus.sh
@@ -71,14 +71,14 @@ fi
 
 # Loop running for 30 times
 # Start loop
-for ((i = 1 ; i <= 30; i++)); do
+for ((i = 1 ; i <= 2; i++)); do
 
     # Copy PDF to home directory
     # so PDF is identical every time
     cp ~/Documents/okular/20yearsofKDE.pdf ~/Documents/20yearsofKDE.pdf
 
     # Burn in time
-    syncUp 60
+    syncUp 10 #60
 
     # Start iteration
     echo "iteration $i;$(date -I) $(date +%T);startTestrun" >> ~/log_sus.csv
@@ -749,7 +749,7 @@ for ((i = 1 ; i <= 30; i++)); do
     rm ~/Documents/20yearsofKDE.pdf
 
     # cool down
-    syncUp 30
+    syncUp 10 #30
 
     clear
 
